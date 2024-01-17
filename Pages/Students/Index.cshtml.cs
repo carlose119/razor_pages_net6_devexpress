285ebd0d1745f7c83d6adfca38d25112bde872d9
carlos_e119 <carlos.e119@gmail.com>
2024-01-17 09:14:42 -0400

    devexpress Razor Page Handlers

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -1,14 +1,17 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using DevExtreme.AspNet.Data;
 using DevExtreme.AspNet.Mvc;
 using DevExtreme.AspNet.Mvc.Builders;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using razor_pages_net6.Data;
 using razor_pages_net6.Models;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace razor_pages_net6.Pages.Student;
 
@@ -40,12 +43,10 @@ public class IndexModel : PageModel
         var courses = (
             from e in _context.Enrollments
             join s in _context.Students on e.StudentID equals s.ID
-            select new
-            {
-                CourseID = e.Course.CourseID,
-                Title = e.Course.Title
-            }
-        ).Distinct().ToList();
+            select new { CourseID = e.Course.CourseID, Title = e.Course.Title }
+        )
+            .Distinct()
+            .ToList();
         var data = new Dictionary<int, string>();
         foreach (var p in courses)
         {
@@ -58,5 +59,53 @@ public class IndexModel : PageModel
     public JsonResult OnGetListStudentsDevExpress()
     {
         return new JsonResult(_context.Students.ToList());
+    }
+
+    // runs on the GET request
+    public IActionResult OnGetGridData(DataSourceLoadOptions loadOptions)
+    {
+        var students = _context.Students.Select(i => new
+        {
+            i.ID,
+            i.LastName,
+            i.FirstMidName,
+            i.EnrollmentDate
+        });
+        return new JsonResult(DataSourceLoader.Load(students, loadOptions));
+    }
+
+    // runs on the PUT request
+    public IActionResult OnPutGridRow(int key, string values)
+    {
+        var model = _context.Students.FirstOrDefault(item => item.ID == key);
+
+        var _values = JsonConvert.DeserializeObject<IDictionary>(values);
+        PopulateModel(model, _values);
+
+        /* if (!TryValidateModel(model))
+            return BadRequest("Validation failed"); */
+
+        _context.SaveChanges();
+        return new OkResult();
+    }
+
+    void PopulateModel(Models.Student model, IDictionary values)
+    {
+        if (values.Contains("ID"))
+        {
+            model.ID = Convert.ToInt32(values["ID"]);
+        }
+        if (values.Contains("LastName"))
+        {
+            model.LastName = Convert.ToString(values["LastName"]);
+        }
+        if (values.Contains("FirstMidName"))
+        {
+            model.FirstMidName = Convert.ToString(values["FirstMidName"]);
+        }
+        if (values.Contains("EnrollmentDate"))
+        {
+            model.EnrollmentDate = Convert.ToDateTime(values["EnrollmentDate"]);
+        }
     }
 }
